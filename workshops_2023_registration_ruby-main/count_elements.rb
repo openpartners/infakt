ddc752d65ce9671226e3065f1504668afcac60df
Andrey <andrzejsew@gmail.com>
2023-10-04 18:42:22 +0200

    Answer from Ruby

--- workshops_2023_registration_ruby-main/count_elements.rb
+++ workshops_2023_registration_ruby-main/count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
-
+    def initialize(array)
+        @array = array
+    end
+    
+    def perform
+        counts = Hash.new(0)
+        @array.each { |element| counts[element] += 1 }
+        counts
+    end
 end
--- workshops_2023_registration_ruby-main/validate_password.rb
+++ workshops_2023_registration_ruby-main/validate_password.rb
@@ -1,3 +1,15 @@
 class ValidatePassword
-
+    def initialize(password)
+        @password = password
+    end
+    
+    def perform
+        return false if @password.length < 6 || @password.length > 24
+        return false unless @password =~ /[A-Z]/
+        return false unless @password =~ /[a-z]/
+        return false unless @password =~ /\d/
+        return false unless @password =~ /[!@#\$%&*+=:;?<>]/
+        return false if @password.scan(/(.)(.*\1){2,}/).any?
+        true
+    end
 end
